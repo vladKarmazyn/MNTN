bd57940056fd747184e80eeffecf541f8772f20a
Vladyslav Karmazyn <karmazynnnn@gmail.com>
2022-08-09 10:12:49 +0300

    TEST

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const mobileButton = document.querySelector('.button');
+const mobileNav = document.querySelector('.mobile_nav');
+
+mobileButton.addEventListener ('click' , function() {
+mobileNav.classList.toggle('mobile_nav-active')
+});
